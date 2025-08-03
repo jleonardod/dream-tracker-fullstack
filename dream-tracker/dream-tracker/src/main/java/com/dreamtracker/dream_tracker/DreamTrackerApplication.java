558304586a9a37d0e4a00e34a57caa309487c9b4
Leonardo Diaz <jldiaz9623@gmail.com>
2025-08-03 07:32:23 -0500

    Inicio del proyecto

---
+++ dream-tracker/dream-tracker/src/main/java/com/dreamtracker/dream_tracker/DreamTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dreamtracker.dream_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamTrackerApplication.class, args);
+	}
+
+}
